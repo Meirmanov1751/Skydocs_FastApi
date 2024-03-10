ae6fa9ce940a85c817fdcc410092f474162f66fd
anvar@realsoft.kz <mTxu[K\Z5\4GFrl*9km):5>
2024-03-10 16:33:03 +0500

    Anvars first commit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from src.brd.test.models import Item
+from sqlalchemy.orm import Session
+from src.brd.test.database import engine, SessionLocal
+
+Item.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
@@ -11,3 +15,13 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+
+@app.post("/items/")
+async def create_item(item: Item, db: Session = Depends(SessionLocal)):
+    db_item = Item(**item.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
