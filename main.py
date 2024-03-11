d2dc20a620ceb7fc1e3c4ddad9755d62d5c285f6
anvar@realsoft.kz <mTxu[K\Z5\4GFrl*9km):5>
2024-03-11 12:59:06 +0500

    Anvars second commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from src.brd.test.models import Item
+from fastapi.params import Depends
 from sqlalchemy.orm import Session
-from src.brd.test.database import engine, SessionLocal
+from src.brd.test.database import engine, SessionLocal, Base
+from src.brd.test.models import Item
 
-Item.metadata.create_all(bind=engine)
+Base.metadata.create_all(engine)
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
@@ -17,7 +19,29 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
+@app.get("/insert/")
+async def insert_data():
+    item = Item(
+        id=2,
+        name="Test2",
+        description="Test Desc2",
+        price=123.55,
+        tax=0.22
+    )
+    s = SessionLocal()
+    s.add(item)
+    s.commit()
+    r = s.query(Item).first()
+    return r
 
+
+@app.get("/inserted/")
+async def inserted():
+    s = SessionLocal()
+    r = s.query(Item).all()
+    return r
+
+'''
 @app.post("/items/")
 async def create_item(item: Item, db: Session = Depends(SessionLocal)):
     db_item = Item(**item.dict())
@@ -25,3 +49,4 @@ async def create_item(item: Item, db: Session = Depends(SessionLocal)):
     db.commit()
     db.refresh(db_item)
     return db_item
+'''
--- src/brd/test/database.py
+++ src/brd/test/database.py
@@ -3,10 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Qwerty12@postgresserver/brd"
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:Qwerty12@localhost:5432/brd"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Qwerty12@localhost:5432/brd"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
+    # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
