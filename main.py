4844d93a5826ebd4fa01dccf86f3a890f4b3188c
anvar@realsoft.kz <mTxu[K\Z5\4GFrl*9km):5>
2024-03-17 14:32:26 +0500

    Anvars commit

--- main.py
+++ main.py
@@ -3,15 +3,34 @@ from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from src.brd.test.database import engine, SessionLocal, Base
 from src.brd.test.models import Item
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 
 Base.metadata.create_all(engine)
 
 app = FastAPI()
 
+origins = [
+    "*",
+    "http://localhost",
+    "http://localhost:9000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    s: str = ""
+    s = "<b>Ввод данных</b><form><input type='text'/></form>"
+    return s
 
 
 @app.get("/hello/{name}")
@@ -19,20 +38,30 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
-@app.get("/insert/")
-async def insert_data():
+class ItemRequest(BaseModel):
+    name: str
+    description: str
+    price: float
+    tax: float
+
+
+@app.post("/insert/")
+async def insert_data(item_request: ItemRequest):
     item = Item(
-        id=2,
-        name="Test2",
-        description="Test Desc2",
-        price=123.55,
-        tax=0.22
+        name=item_request.name,
+        description=item_request.description,
+        price=item_request.price,
+        tax=item_request.tax
     )
-    s = SessionLocal()
-    s.add(item)
-    s.commit()
-    r = s.query(Item).first()
-    return r
+
+    # Сохранение объекта в базе данных
+    session = SessionLocal()
+    session.add(item)
+    session.commit()
+
+    # Возврат сохраненного объекта
+
+    return item
 
 
 @app.get("/inserted/")
@@ -40,6 +69,21 @@ async def inserted():
     s = SessionLocal()
     r = s.query(Item).all()
     return r
+
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    s = SessionLocal()
+    s.query(Item).filter(Item.id == item_id).delete()
+    s.commit()
+    print("delete item: ", item_id)
+    return
+
+
+@app.delete("/delete_items/")
+async def delete_item(items):
+    print("delete item: ", items)
+    return
 
 '''
 @app.post("/items/")
