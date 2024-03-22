3773c2d575e7ca9b649b6531bc2c952250c0fe2f
anvar@realsoft.kz <mTxu[K\Z5\4GFrl*9km):5>
2024-03-22 18:14:18 +0500

    Anvars commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.responses import JSONResponse
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from src.brd.test.database import engine, SessionLocal, Base
-from src.brd.test.models import Item
+from src.brd.test.models import Item, Attachment
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
@@ -52,10 +52,50 @@ async def upload_file(files: List[UploadFile] = File(...)):
     print("2upload_file started files:", files)
     count = 0
     try:
+        # Сохранение объекта в базе данных
+        session = SessionLocal()
+
         for file in files:
             count = count + 1
-            print("2upload_file file", count, file)
-            print("2upload_file content_type", file.content_type)
+            print("3upload_file file", count, file)
+            print("3upload_file content_type", file.content_type)
+            data = await file.read()
+            file_location = f"d:/files/{file.filename}"
+            with open(file_location, "wb+") as file_object:
+                file_object.write(data)
+            '''    
+            print("3upload_file data len", len(data))
+            try:
+                print("3upload_file save start")
+                file_location = f"d:/files/{file.filename}"
+                print("3upload_file file_location", file_location)
+                with open(file_location, "wb+") as file_object:
+                    file_object.write(file.file.read())
+                    print("2contents save to: ", file_location)
+                print("3upload_file save finish")
+                contents = file.file.read()
+                with open(file.filename, 'wb') as f:
+                    f.write(contents)
+                print("0 file.filename:", file.filename, "contents len: ", len(contents), ": ", contents)
+                #  print("2contents: ", contents)
+            except Exception:
+                return {"message": "There was an error uploading the file(s)"}
+            finally:
+                file.file.close()
+            print("1 file.filename:", file.filename, "contents len: ", len(contents))
+            '''
+
+            attachment = Attachment(
+                name=file.filename,
+                size=file.size,
+                data=data
+            )
+
+            session.add(attachment)
+
+        session.commit()
+
+
         # Здесь вы можете обработать загруженный файл, например, сохранить его на сервере
         # или выполнить другие действия с ним
         ''' print(f'filename>>> {file.filename}')
@@ -91,6 +131,22 @@ async def inserted():
     s = SessionLocal()
     r = s.query(Item).all()
     return r
+
+
+@app.get("/attachments/")
+async def get_att_all():
+    s = SessionLocal()
+    r = s.query(Attachment).all()
+    return r
+
+
+@app.get("/attachments/{att_id}")
+async def get_att_by_id(att_id: int):
+    s = SessionLocal()
+    r = s.query(Attachment).filter(Attachment.id == att_id).first()
+    ba: bytearray = r.data
+    print("att_id", att_id, "file", r.name, "data len", len(ba))
+    return r.name
 
 
 @app.delete("/items/{item_id}")
--- src/brd/test/models.py
+++ src/brd/test/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float
-
+from sqlalchemy import Column, Integer, String, Float, BigInteger
+from sqlalchemy.dialects.postgresql import OID, BYTEA
 from src.brd.test.database import Base
 
 
@@ -11,3 +11,12 @@ class Item(Base):
     description = Column(String, index=True, nullable=True)
     price = Column(Float)
     tax = Column(Float, nullable=True)
+
+
+class Attachment(Base):
+    __tablename__ = "attachments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    size = Column(BigInteger, index=True)
+    data = Column(BYTEA)
